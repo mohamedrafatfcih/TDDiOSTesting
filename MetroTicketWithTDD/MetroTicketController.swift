6eee950476a8dc8d26a68a30bc4a86cdfd512a9d
Mohamed Rafat <mohamedfafatfcih@gmail.com>
2022-07-14 06:00:42 +0200

    Implement metro ticket controller

--- MetroTicketWithTDD/MetroTicketController.swift
+++ MetroTicketWithTDD/MetroTicketController.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+class MetroTicketController{
+    var oldPeopleTickets: [MetroTicket]?
+    var teenageTickets: [MetroTicket]?
+    var childrenTickets: [MetroTicket]?
+    
+    
+    init(){
+        oldPeopleTickets = [MetroTicket]()
+        teenageTickets = [MetroTicket]()
+        childrenTickets = [MetroTicket]()
+    }
+    
+    func addOldPeopleTicket(_ ticket: MetroTicket){
+        oldPeopleTickets?.append(ticket)
+    }
+    
+    func addTeenageTicket(_ ticket: MetroTicket){
+        teenageTickets?.append(ticket)
+    }
+    
+    func addChildrenTicket(_ ticket: MetroTicket){
+        childrenTickets?.append(ticket)
+    }
+}
